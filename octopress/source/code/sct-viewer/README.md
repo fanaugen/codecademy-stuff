72c90048b2ca6e873d3db7ec57b10f63e631d7d4
Alexander Jahraus <fanaugen@me.com>
2013-04-08 23:20:38 +0200

    write README for SCT Viewer

--- octopress/source/code/sct-viewer/README.md
+++ octopress/source/code/sct-viewer/README.md
@@ -1,1 +1,30 @@
-# Submission Correctness Test Viewer Bookmarklet
+---
+layout: page
+title: "SCT Viewer"
+comments: true
+sharing: true
+---
+
+This JavaScript bookmarklet is based on the [original][orig] Submission Correctness Viewer bookmarklet by [minrice][min] (Michael Tackes). My version should work with Codecademy’s [new coding UI][ui].
+
+It uses Twitter Bootstrap’s [Modal][modal] component (the new UI already loads the CSS styles for that; the javascript part is loaded dynamically on first use).
+
+[orig]: http://www.codecademy.com/users/4f190e333aa50b000100aeb9/projects/500c7558d143430002002551/
+[min]:  http://www.codecademy.com/users/minrice2099
+[ui]:   http://www.codecademy.com/blog/62-introducing-a-new-learning-experience
+[modal]: http://twitter.github.io/bootstrap/javascript.html#modals
+
+The bookmarklet only works on Codecademy exercise pages (like [this one][ex]). Drag the link below to your bookmarks (keeping it in the Bookmarks bar gives you one-click access), then use it in any Codecademy exercise.
+
+[ex]: http://www.codecademy.com/courses/javascript-lesson-n1woS/1/1
+
+<div style="text-align:center;background-color:lightyellow;vertical-align:middle;height:3em;padding:.8em;">
+	<p><a title="Add this to your Bookmarks" href="#placeholder">Show SCT</a></p>
+</div>
+
+**There may be bugs** and I haven’t tested it on any browser other than the latest Chrome for Mac. So if you find any errors, do let me know.
+
+## Source
+{% include_code SCT Viewer sct-viewer/sct-viewer.js %}
+
+Now go try it out!
---
+++ octopress/source/code/sct-viewer/sct-viewer.js
@@ -0,0 +1,61 @@
+/*
+Display SCT (Submission Correctness Test) of a Codecademy Exercise.
+By Alex Jahraus (based on the original SCT Viewer Bookmarklet by Michael Tackes)
+Works only in Codecademy’s New UI, see
+http://www.codecademy.com/blog/62-introducing-a-new-learning-experience
+Dependencies:
+  - jQuery, Twitter Bootstrap’s CSS for modals and highlight.js (loaded by Codecademy)
+  - bootstrap-modal.js (loaded dynamically)
+How To Use: the minified code can be used in a bookmarklet to execute
+the script on the page of the exercise you’re currently viewing.
+*/
+
+(function(){
+
+try {
+  // step 1: get the code and some info
+  var i    = location.pathname.split('/')[4] - 1,
+      proj = CCDATA.composer.current_project,
+      auth = proj.author.handle,
+      lang = proj.language.toLowerCase(),
+      sct  = proj.checkpoints[i].test_functions;
+
+  // step 2: load bootstrap-modal.js
+  if ($.fn.modal) do_the_magic()
+  else $.getScript('//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap'+
+  '/2.3.1/js/bootstrap-modal.js')
+  .done(do_the_magic)
+  .fail(function(r,s,e){throw e})
+} catch (e) {
+  return console.log(e.message);
+}
+function do_the_magic(){
+  // step 3: compose the modal div
+  var $modal, modal_exists = $('#sct').length > 0;
+  if (modal_exists) { // the modal is already in the document
+    $modal = $('#sct');
+  } else {       // create the modal
+    $modal = $('<div id=\'sct\' class=\'modal fade hide\' style=\'width:680px\'>')
+      .append($('<div class=\'modal-header\'>')
+        .append($('<button type=\'button\' class=\'close\' data-dismiss=\'modal\'>').text('×'))
+        .append('<h3>Correctness Test <small> &nbsp; by '+auth+'</small></h3>')
+    ).append($('<div class=\'modal-body\'>')
+      .append($('<pre>').html('<code></code>'))
+    )
+    .append($('<div class=\'modal-footer\'>')
+      .append('<a target=\'blank\' href=\'//www.codecademy.com/docs/submission_tests\'>About Correctness Tests</a> &nbsp;|&nbsp; <a target=\'blank\' href=\'#\'>Feedback</a> &nbsp;')
+      .append($('<button class=\'btn btn-small\' data-dismiss=\'modal\'>').text('Close'))
+    ).appendTo($('body'));
+  }
+
+  // step 4: sanitize, insert, and highlight the code
+  sct = sct.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;')
+  var $code = $modal.find('code').eq(0)
+    .attr('class','language-'+ (lang == 'web' ? 'javascript' : lang))
+    .html(sct)
+  hljs && hljs.highlightBlock($code.get(0))
+
+  // step 5: show the modal
+  $modal.modal('show')
+}
+})();
